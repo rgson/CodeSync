fefc70d4ab47b11991f34a96d94669d024d29793
Linus <linus@ubuntu.(none)>
2015-02-12 11:18:37 +0100

    Changed the login page to be the first page (3)

--- website/resources/views/app.blade.php
+++ website/resources/views/app.blade.php
@@ -28,17 +28,15 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="/">Home</a></li>
+					<li><a href="/">"Home", ska leda till vart och finnas när?</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="/auth/login">Login</a></li>
 						<li><a href="/auth/register">Register</a></li>
 					@else
 						<li class="dropdown">
