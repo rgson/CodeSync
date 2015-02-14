c478867b78772cffd5650319124a67c4e7fffdd5
Linus <linus@ubuntu.(none)>
2015-02-14 17:23:22 +0100

    Added files

---
+++ website/database/migrations/2015_02_12_174115_create_projacc_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjaccTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projacc', function(Blueprint $table)
+		{
+			$table->integer('projid')->length(10)->unsigned();
+			$table->integer('userid')->length(10)->unsigned();
+			$table->primary(array('projid', 'userid'));			
+			$table->timestamps();	
+			$table->foreign('projid')->references('id')->on('projects');
+			$table->foreign('userid')->references('id')->on('users');
+		});
+
+		Schema::table('projacc', function(Blueprint $table)
+		{
+			
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projacc');
+	}
+
+}
